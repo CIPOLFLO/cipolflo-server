f86595ea331b7dcd8e3a334d1899b43a9e031e62
Camila <ca229863@fi365.ort.edu.uy>
2026-05-27 18:58:09 -0300

    prueba cambio version de springboot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
 	repositories {
-		maven { url = uri( "https://repo.spring.io/snapshot") }
+		maven { url = uri("https://repo.spring.io/release") }
 		gradlePluginPortal()
 		mavenCentral()
 	}
