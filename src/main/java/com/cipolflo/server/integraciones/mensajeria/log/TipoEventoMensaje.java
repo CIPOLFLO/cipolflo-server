201d28992e4a6efb6a1abe03c6dbf308d85eedbc
Camila <ca229863@fi365.ort.edu.uy>
2026-07-13 00:10:12 -0300

    se quitan cosas relacionadas a notificaciones para telegram

--- src/main/java/com/cipolflo/server/integraciones/mensajeria/log/TipoEventoMensaje.java
+++ src/main/java/com/cipolflo/server/integraciones/mensajeria/log/TipoEventoMensaje.java
@@ -3,6 +3,5 @@ package com.cipolflo.server.integraciones.mensajeria.log;
 /** Qué originó el envío de un mensaje, para poder auditar después qué se mandó. */
 public enum TipoEventoMensaje {
     RESPUESTA_CONSULTA,
-    NOTIFICACION_INTERNA,
     RECHAZO_NO_AUTORIZADO
 }
